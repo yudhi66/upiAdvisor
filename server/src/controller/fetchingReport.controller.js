dd201cb834945d73c026c17674e3c210277badc6
yudhi66 <yudhivikram536@gmail.com>
2024-12-17 01:45:25 +0530

    fetchingReport

---
+++ server/src/controller/fetchingReport.controller.js
@@ -0,0 +1,42 @@
+import { ApiError } from "../utils/ApiError.js";
+
+import { asyncHandler } from "../utils/asyncHandler.js";
+ 
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Upi } from "../models/upi.model.js";
+import extractBaseUpi from "../utils/baseUpi.js";
+import isValidUpi from "../utils/validUpi.js";
+
+
+
+
+
+const fetchReport=asyncHandler(async (req,res)=>{
+   
+    const {upi}=req.body;
+  
+
+    if(!isValidUpi(upi)){
+        throw new ApiError(401,"Invalid upi format")
+    }
+
+    const baseUpi=extractBaseUpi(upi);
+    const existingUpi=await Upi.findOne({baseUpi}).select("-reportedBy") 
+    console.log(existingUpi)
+
+    if(!existingUpi){
+        return res.status(207).json(
+            new ApiResponse(207,{},"Not Reported till now if suspicous please report")
+        )
+    }else{
+        return res.status(200).json(
+            new ApiResponse(200,existingUpi,"Report fetched") 
+        )
+
+    }
+
+})
+
+
+export {fetchReport};
+
--- server/src/routes/upi.routes.js
+++ server/src/routes/upi.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { reporting } from "../controller/reporting.controller.js";
 
+import {fetchReport } from "../controller/fetchingReport.controller.js";
 const router=Router();
 
 
 router.route("/report").post(verifyJWT,reporting);
+
+router.route("/fetchReport").post(fetchReport)
 
 
 
