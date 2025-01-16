5edd8c93383363216a3464e18ef2bf86ecaf8b8e
yudhi66 <yudhivikram536@gmail.com>
2025-01-17 02:48:53 +0530

    cookie options

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -181,8 +181,7 @@ const getUser=asyncHandler(async (req,res)=>{
   console.log("hello");
 
   const incomingRefreshToken=req.cookies.refreshToken;
-   
-
+ 
   if(!incomingRefreshToken){
    return res.status(401).json(new ApiResponse(401, {}, "Not logged in"));
   }
@@ -195,12 +194,15 @@ const getUser=asyncHandler(async (req,res)=>{
       return res.status(401).json(new ApiResponse(401, {}, "Not logged in"));
     }
     
+    const isProduction = process.env.NODE_ENV === "production";
+
     const accessToken=user.generateAccessToken();
     const options={
-      httpOnly:true,
-      secure:true
+      httpOnly: true,
+  secure: isProduction,  
+  sameSite: isProduction ? "None" : "Lax", 
      }
-
+   
      return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",incomingRefreshToken,options).json(
       new ApiResponse(
         200,
